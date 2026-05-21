8e9b2cb1a9320aba3e91f74d3791d63114fa935f
Niffoxic <niffoisme@gmail.com>
2026-05-21 19:24:42 +0100

    Add shader storage interface

---
+++ engine/include/engine/graphics/shaders/storage/shader_storage.h
@@ -0,0 +1,34 @@
+// Created by Niffoxic (Harsh Dubey)
+#ifndef CURSEOFTHESEA_SHADER_STORAGE_H
+#define CURSEOFTHESEA_SHADER_STORAGE_H
+
+#include <cstdint>
+#include <string>
+#include <vector>
+#include <unordered_map>
+
+namespace cots::graphics::shaders
+{
+    struct shader_cache_entry
+    {
+        std::uint64_t             key         { 0 };
+        std::uint64_t             source_hash { 0 };
+        std::string               identifier  {};
+        std::vector<std::uint8_t> dxil        {};
+    };
+
+    using cache_map = std::unordered_map<std::uint64_t, shader_cache_entry>;
+
+    //~ swappable persistence policy
+    class shader_storage
+    {
+    public:
+        virtual ~shader_storage() = default;
+
+        //~ cold cache
+        [[nodiscard]] virtual bool load_all (cache_map& out)       = 0;
+        [[nodiscard]] virtual bool store_all(const cache_map& in)  = 0;
+    };
+}
+
+#endif
